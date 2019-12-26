a59e500f0a98e8a3307aa8a36d88b727c85a2bbf
Paul Novak <paul.novak@schrodinger.com>
2019-12-26 07:51:18 -0500

    more day7

--- 2019/day/7/intcode_computer_3.0.js
+++ 2019/day/7/intcode_computer_3.0.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const _ = require('lodash')
 const stdin = process.stdin,
       stdout = process.stdout,
@@ -10,6 +11,7 @@ stdin.on('data', function (chunk) {
   inputChunks.push(chunk);
 });
 
+const DEBUG = 1
 const [ADD, MULTIPLY, INPUT, OUTPUT, JUMP_IF_TRUE, JUMP_IF_FALSE, LESS_THAN, EQUALS, STOP] = [1, 2, 3, 4, 5, 6, 7, 8, 99]
 const [POSITION_MODE, IMMEDIATE_MODE] = ['0', '1']
 const TARGET_OUTPUT = 19690720
@@ -20,64 +22,69 @@ const AMPS = [0,1,2,3,4]
 const TEST1 = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0'
 const TEST2 = '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0'
 const TEST3 = '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0'
+const TEST7B1 = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
 
 stdin.on('end', function () {
+  console.log('testing...')
+
+  // assert.equal(amplify(TEST1, [4,3,2,1,0]), 43210)
+  // assert.equal(largestOutput(TEST2, AMPS), 54321)
+  // assert.equal(largestOutput(TEST3, AMPS), 65210)
+
 
   const text = inputChunks.join()
   // const ac = execute(text, [AC])
   // console.log(`test ac: ${ac}`)
   // const thermalRadiator = execute(text,[THERMAL_RADIATOR])
   // console.log(`test thermal radiator ${thermalRadiator}`)
+  //const part1Result = largestOutput(text, AMPS)
+  //assert.equal(part1Result, 422858)
+  //console.log(`part1Result: ${part1Result}`)
 
-  const testAmplify1 = amplify(TEST1, [4,3,2,1,0])
-  console.log(`testAmplify1 ${testAmplify1}`)
-
-  const test1 = largestOutput(TEST1, AMPS)
-  const test2 = largestOutput(TEST2, AMPS)
-  const test3 = largestOutput(TEST3, AMPS)
-  console.log(`test1 ${test1}`)
-  console.log(`test2 ${test2}`)
-  console.log(`test3 ${test3}`)
-
-  const part1Result = largestOutput(text, AMPS)
-  console.log(`part1Result: ${part1Result}`)
-
-  const test4 = largestOutput(text, [9,8,7,6,5])
-  console.log(`test4 ${test4}`)
+  assert.equal(execute(TEST7B1.split(','), [9,8,7,6,5]), 139629729)
+  const test7b1 = largestOutput(TEST7B1, [5,6,7,8,9])
+  console.log(`test7b1 ${test7b1}`)
 });
 
 const getOperand = (operandRef, paramMode, codes) => {
-  if (paramMode !== IMMEDIATE_MODE) {
-    return parseInt(codes[operandRef], 10)
+
+  let result
+  if (paramMode === POSITION_MODE) {
+    console.log('position mode')
+    result = parseInt(codes[operandRef], 10)
+  } else {
+    console.log('immediate mode')
+    result = parseInt(operandRef, 10)
   }
-  return parseInt(operandRef, 10)
+  if (DEBUG) console.log(`getOperand operandRef: ${operandRef} paramMode: ${paramMode} result: ${result}`)
+  return result
 }
 
 const add = ({pointer, codes, operand1, operand2, outputRef}) => {
-  //console.log(`ADD ${operand1} + ${operand2} setting outputRef ${outputRef} to ${operand1 + operand2}`)
+  if (DEBUG) console.log(`ADD ${operand1} + ${operand2} setting outputRef ${outputRef} to ${operand1 + operand2}`)
   codes[outputRef] = (operand1 + operand2).toString()
   return pointer + 4
 }
 
 const multiply =  ({pointer, codes, operand1, operand2, outputRef}) => {
-  //console.log(`MULTIPLY ${operand1} * ${operand2} setting outputRef ${outputRef} to ${operand1 * operand2}`)
+  if (DEBUG) console.log(`MULTIPLY ${operand1} * ${operand2} setting outputRef ${outputRef} to ${operand1 * operand2}`)
   codes[outputRef] = (operand1 * operand2).toString()
   return pointer + 4
 }
 
-const input = ({pointer, codes, ref, value}) => {
-  codes[ref] = value.toString()
-  //console.log(`INPUT setting ref: ${ref} to ${value}`)
+const input = ({pointer, codes, operand1Ref, value}) => {
+  if (DEBUG) console.log(`INPUT setting ref: ${operand1Ref} to ${value}`)
+  codes[operand1Ref] = value.toString()
   return pointer + 2
 }
 
 const output = ({pointer, codes, value}) => {
-  //console.log(`OUTPUT ${value}`)
+  if (DEBUG) console.log(`OUTPUT ${value}`)
   return pointer + 2
 }
 
 const jumpIfTrue = ({ pointer, codes, operand1, operand2}) => {
-  //console.log(`jump if true`)
+  if (DEBUG) console.log(`JUMP_IF_TRUE operand1: ${operand1} operand2: ${operand2}` )
   if (operand1 !== 0 ) {
     return operand2
   }
@@ -85,6 +92,7 @@ const jumpIfTrue = ({ pointer, codes, operand1, operand2}) => {
 }
 
 const jumpIfFalse = ({ pointer, codes, operand1, operand2}) => {
+  if (DEBUG) console.log('JUMP_IF_FALSE')
   if (operand1 === 0) {
     return operand2
   } else {
@@ -100,7 +108,7 @@ const lessThan = ( { pointer, codes, operand1, operand2, outputRef }) => {
     value = 0
   }
   codes[outputRef] = value
-  //console.log(`LESS_THAN ${operand1} < ${operand2} setting ${outputRef} to ${value}`)
+  if (DEBUG) console.log(`LESS_THAN ${operand1} < ${operand2} setting ${outputRef} to ${value}`)
   return pointer + 4
 }
 
@@ -112,10 +120,11 @@ const equals = ( { pointer, codes, operand1, operand2, outputRef }) => {
     value = 0
   }
   codes[outputRef] = value
-  //console.log(`EQUALS ${operand1} === ${operand2} setting ${outputRef} to ${value}`)
+  if (DEBUG) console.log(`EQUALS ${operand1} === ${operand2} setting ${outputRef} to ${value}`)
   return pointer + 4
 }
 const parseCommand = (pointer, codes) => {
+  if (DEBUG) console.log(_.reduce(codes, (result, value, idx) => {result[idx] = value; return result;},{}))
   const opCode = codes[pointer]
   const operation = parseInt(opCode.slice(-2), 10)
   const paramMode1 = opCode.slice(-3, -2) || '0'
@@ -124,7 +133,7 @@ const parseCommand = (pointer, codes) => {
   const operand1Ref = parseInt(codes[pointer + 1], 10)
   const operand2Ref = parseInt(codes[pointer + 2], 10)
   const outputRef = parseInt(codes[pointer + 3], 10)
-  //console.log(`pointer: ${pointer} opCode: ${opCode} operation: ${operation} paramMode1: ${paramMode1} paramMode2: ${paramMode2} paramMode3: ${paramMode3} operand1Ref: ${operand1Ref} operand2Ref: ${operand2Ref} outputRef: ${outputRef} `)
+  if (DEBUG) console.log(`pointer: ${pointer} opCode: ${opCode} operation: ${operation} paramMode1: ${paramMode1} paramMode2: ${paramMode2} paramMode3: ${paramMode3} operand1Ref: ${operand1Ref} operand2Ref: ${operand2Ref} outputRef: ${outputRef} `)
   return {opCode, operation, paramMode1, paramMode2, paramMode3, operand1Ref, operand2Ref, outputRef}
 }
 
@@ -143,10 +152,10 @@ const permutations = function*(elements) {
   }
 }
 
-const execute = (text, inputValues) => {
-  const codes = text.split(',')
+const execute = (codes, inputValues) => {
   let i = 0
   let inputPointer = 0
+
   while(i < codes.length) {
     const  {opCode, operation, paramMode1, paramMode2, paramMode3, operand1Ref, operand2Ref, outputRef} =
           parseCommand(i, codes)
@@ -164,16 +173,17 @@ const execute = (text, inputValues) => {
       continue
     }
     if (operation === INPUT) {
-      const ref = parseInt(codes[pointer + 1], 10)
-      i = input({pointer, codes, ref, value: inputValues[inputPointer]})
+      const operand1 = getOperand(operand1Ref, paramMode1, codes)
+      i = input({pointer, codes, operand1Ref, value: inputValues[inputPointer]})
       inputPointer++
+
       continue
     }
     if (operation === OUTPUT) {
       const value = getOperand(operand1Ref, paramMode1, codes)
       outputValue = value
       i = output({pointer, codes, value})
-      continue
+      break
     }
     if (operation === JUMP_IF_TRUE) {
       const operand1 = getOperand(operand1Ref, paramMode1, codes)
@@ -200,31 +210,40 @@ const execute = (text, inputValues) => {
       continue
     }
     if (operation === STOP) {
-      //console.log('STOP')
-      return outputValue
+      if (DEBUG) console.log('STOP')
+      break
+
     } else {
       console.log(`invalid operation: ${operation}`)
       return
     }
   }
+
+  return outputValue
 }
 
 const largestOutput = (code, phases) => {
   const phaseSettings = permutations(phases)
   let max = -Infinity
   for (const phaseSetting of phaseSettings) {
-    //console.log(phaseSetting)
+    if (DEBUG) console.log(phaseSetting)
     const value = amplify(code, phaseSetting)
     max = max < value ? value : max
   }
   return max
 }
 
-const amplify = (code, phases) => {
+const amplify = (text, phases) => {
+  const codes = text.split(',')
+
   let value = 0
-  _.forEach(phases, phase => {
-    value = execute(code, [phase, value])
-    //console.log(value)
-  })
+  let i = 0
+  while (true) {
+    const phase = phases[i%phases.length]
+    value = execute(codes, [phase, value])
+    if (DEBUG) console.log(value)
+    i++
+  }
+
   return value
 }
